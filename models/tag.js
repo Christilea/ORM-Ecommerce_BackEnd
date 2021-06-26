8db4171fc01639ca664fe9eca7c730187118f95f
Christi <mahomes4president@gmail.com>
2021-06-25 19:23:53 -0500

    changes to models folder

--- models/tag.js
+++ models/tag.js
@@ -13,24 +13,13 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    product_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "product",
-        key: "id",
-      },
+    tag_name: {
+      type: DataTypes.STRING,
+      
     },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "tag",
-        key: "id",
-      },
-    },
+    
   },
-  {
+  { 
     sequelize,
     timestamps: false,
     freezeTableName: true,
